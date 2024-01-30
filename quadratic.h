d135af2adc141a85111946181c700a646e790485
Sunny Kim <154963758+sskeme@users.noreply.github.com>
2024-01-30 18:27:54 -0500

    Create quadratic.h

---
+++ quadratic.h
@@ -0,0 +1,24 @@
+#ifndef QUADRATIC_H__ // header guard
+#define QUADRATIC_H__
+
+/**
+ * Compute value of a quadratic
+ *
+ * @param x - input
+ * @param a - coefficient a
+ * @param b - coefficient b
+ * @param c - coefficient c
+ * @return y(x) = ax^2 + bx +c
+ */
+double quad(double x, double a, double b, double c);
+
+/**
+ * Compute value of the derivative of a quadratic
+ * @param x - input
+ * @param a - coefficient a
+ * @param b - coefficient b
+ * @return y'(x) = 2ax + b
+ */
+double quadPrime(double x, double a, double b);
+
+#endif
